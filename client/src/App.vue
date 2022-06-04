57399e3cf45d08823c7d4e78dfaf3873a5fb45f1
Jeremy Eisenman <jeisenman5000@gmail.com>
2022-06-03 22:33:43 -0400

    front end

---
+++ client/src/App.vue
@@ -0,0 +1,19 @@
+<template>
+  <div id="app" class="app">
+    <Homepage />
+  </div>
+</template>
+
+<script>
+import Homepage from './components/Homepage'
+
+export default {
+  name: 'App',
+
+  components: {
+    Homepage
+  }
+}
+</script>
+
+
---
+++ client/src/components/Homepage.vue
@@ -0,0 +1,15 @@
+<template>
+  <div>
+    Hello world
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Homepage',
+
+  mounted() {
+    console.log("HELLO");
+  }
+}
+</script>
